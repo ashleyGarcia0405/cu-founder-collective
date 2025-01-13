1958785764f4514450838dc448cfbb8ce6782de8
Ashley Garcia <ashley.garcia.official@gmail.com>
2025-01-13 17:15:34 -0500

    fix spline tool

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from "react";
-import SplineAnimation from "./components/SplineAnimation";
+import SplineAnimation from "./components/spline_animations";
 
 
 
